3c246407ff422a542b8ed899f824dc3067be66e5
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-02-16 11:14:38 +0300

    add tests for src/Utils.php

---
+++ tests/UtilsTests.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hexlet\Phpunit\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Hexlet\Phpunit\Utils\reverseString;
+
+class UtilsTest extends TestCase
+{
+    public function testReverse(): void
+    {
+        $this->assertEquals('', reverseString(''));
+        $this->assertEquals('olleh', reverseString('hello'));
+    }
+}
